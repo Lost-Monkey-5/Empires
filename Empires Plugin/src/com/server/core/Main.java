84b3422c5f3fdcc35148434510de45f1c48c00b2
Samuel Dauchert <lostlonkey5@gmail.com>
2019-01-07 17:36:34 -0500

    First plugin runs on server!

--- Empires Plugin/src/com/server/core/Main.java
+++ Empires Plugin/src/com/server/core/Main.java
@@ -1,5 +1,14 @@
 package com.server.core;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		System.out.println("Starting First Plugin!");
+	}
+	@Override
+	public void onDisable() {
+		
+	}
 }
