8a0fb81cb6df5208e6ee1c0f3d388590cc929e7f
Samuel Dauchert <lostlonkey5@gmail.com>
2019-01-08 00:32:26 -0500

    Added Event Handlers to the main. Added a basic permission check from the Event Handler. Created a default config.yml file to the plugin and a command to print the variables from the config.

--- Empires Plugin/src/com/empires/core/Main.java
+++ Empires Plugin/src/com/empires/core/Main.java
@@ -1,31 +1,56 @@
 package com.empires.core;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin{
+public class Main extends JavaPlugin implements Listener{
 	@Override
 	public void onEnable() {
 		System.out.println("Pugin Enabled!");
+		
+		this.getConfig().options().copyDefaults();
+		saveDefaultConfig();
+		
+		Bukkit.getPluginManager().registerEvents(this, this);
 	}
 	@Override
 	public void onDisable() {
 		System.out.println("Plugin Disabled!");
 	}
-	
-	public boolean onCommand(CommandSender sender, Command cmd, String lable, String[] args) {
-		if(cmd.getName().equals("hello")) {
-			if(sender instanceof Player) {
-				Player player = (Player) sender;
-				player.sendMessage(ChatColor.GRAY + "Hello, " + ChatColor.GREEN + player.getName() + ChatColor.GRAY + ". Your Health has been restored!");
-				player.setHealth(20.0);
-			}else {
-				System.out.println("You cannot use this unless you are a player!");
-			}
+	@EventHandler
+	public void onMove(PlayerMoveEvent e) {
+		Player p = e.getPlayer();
+		
+		if(!p.hasPermission("core.allowmove")) {
+			p.sendMessage("You are Petrified!");
+			e.setCancelled(true);
 		}
+	}
+	@EventHandler
+	public void onThrow(PlayerEggThrowEvent e) {
+		Player p = e.getPlayer();
+		p.sendMessage(ChatColor.DARK_RED + "STOP THROWING MY BLOODY EGGS!");
+	}
+	public boolean onCommand(CommandSender sender, Command cmd, String lable, String[] args) {
+		
+		Player p = (Player) sender;
+		
+		if(cmd.getName().equals("config")) {
+			String word = this.getConfig().getString("Word");
+			int number = this.getConfig().getInt("Number");
+			
+			p.sendMessage(ChatColor.DARK_GRAY + "The word is " + ChatColor.GREEN + word 
+					+ ChatColor.DARK_GRAY + ". The number is " + ChatColor.DARK_GREEN 
+					+ number + ChatColor.GRAY + ".");
+		}		
 		return false;
 	}
 }
