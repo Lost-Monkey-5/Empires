ebd12c46d535c9ade4afb1a3864edb27f96d2a93
Samuel Dauchert <lostlonkey5@gmail.com>
2019-01-10 00:54:08 -0500

    Added a title using NMS Packets! From the first tutorial on NMS.

--- Empires Plugin/src/com/empires/core/SpawnListener.java
+++ Empires Plugin/src/com/empires/core/SpawnListener.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -13,12 +14,42 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 
+import net.minecraft.server.v1_13_R2.IChatBaseComponent.ChatSerializer;
+import net.minecraft.server.v1_13_R2.PacketPlayOutTitle;
+import net.minecraft.server.v1_13_R2.PacketPlayOutTitle.EnumTitleAction;
+
 public class SpawnListener implements Listener {
 	
 	@EventHandler
-	public void onSpawn(PlayerJoinEvent e) {
+	public void onSpawn(PlayerJoinEvent event) {
 		
-		//Create player
+		displayJoinTitle(event);
+		spawnMaxTheZombie(event);
+	}
+	private void displayJoinTitle(PlayerJoinEvent e) {
+		//PacketPlayOutTitle(arg0, arg1, arg2, arg3, arg4)
+		//arg0 EnumTitleAction
+		//arg1 ChatSerializer
+		//arg2 Number of ticks for fade-in phase of title
+		//arg3 Number of ticks for stay phase of title
+		//arg4 Number of ticks for fade-out phase of title
+		
+		//Create Title Packet
+		PacketPlayOutTitle titlePacket = new PacketPlayOutTitle(EnumTitleAction.TITLE, 
+				ChatSerializer.a("{\"text\":\"" + " §o§4Lost's World" + "\"}"), 
+				20, 100, 10);
+		//Create Subtitle Packet
+		PacketPlayOutTitle subTitlePacket = new PacketPlayOutTitle(EnumTitleAction.SUBTITLE, 
+				ChatSerializer.a("{\"text\":\"" + " §8A midieval adventure awaits §0" 
+				+ e.getPlayer().getName() + "§8!" + "\"}"), 
+				20, 100, 10);
+		//Send Title Packet
+		((CraftPlayer) e.getPlayer()).getHandle().playerConnection.sendPacket(titlePacket);
+		//Send Sub-Title Packet
+		((CraftPlayer) e.getPlayer()).getHandle().playerConnection.sendPacket(subTitlePacket);
+	}
+	private void spawnMaxTheZombie(PlayerJoinEvent e) {
+		//Get the Player for joined member
 		Player p = (Player) e.getPlayer();
 		//Get players Location
 		Location playerLocation = p.getLocation();
